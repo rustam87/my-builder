eb76f2958f84e146890c1a12e2d005f5a9af6f3f
rustam <abrus1987@gmail.com>
2017-07-09 14:47:53 +0300

    add style and logic for btn menu

--- source/js/app.js
+++ source/js/app.js
@@ -1,11 +1,13 @@
-(function() {
-  'use strict';
-  var btnMenu  = document.querySelector('.btn-menu');
-  var siteMenu = document.querySelector('.site-menu');
-      //.classList.add('m--show');
+(function () {
+    'use strict';
+    var btnMenu = document.querySelector('.btn-menu');
+    var siteMenu = document.querySelector('.site-menu');
 
-  btnMenu.addEventListener('click', function() {
-    siteMenu.classList.add('open');
-  });
+    btnMenu.addEventListener('click', function () {
+        var windowHeight = window.outerHeight;
+        siteMenu.style.height = windowHeight + 'px';
+        document.body.classList.toggle('open-menu');
+    });
 
 })();
+
