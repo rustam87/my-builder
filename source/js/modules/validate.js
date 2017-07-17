240908d66a78bb4ac1b42a08b2e66ffe6719de7e
rustam <abrus1987@gmail.com>
2017-07-17 15:51:38 +0300

    add validate to callback form

--- source/js/modules/validate.js
+++ source/js/modules/validate.js
@@ -10,76 +10,33 @@ class Validate {
                 if(!isValid) {
                     return false;
                 }
-
             });
         }
     }
 
     validate(form){
+
         let fields = form.querySelectorAll('[data-valid]');
         let isValid = true;
 
         [].forEach.call(fields, (field) => {
+            let fieldBlock = field.closest('.form-control');
+            let errorFieldBlock = fieldBlock.querySelector('[data-error-block]');
 
-            //if (field.type === 'text' || field.type === 'password') {
-                let fieldBlock = field.closest('.form-control');
-                let errorFieldBlock = fieldBlock.querySelector('[data-error-block]');
+            if (field.value) {
+                fieldBlock.classList.remove('form-control_error');
+                errorFieldBlock.classList.remove('error-message_open');
+                return;
+            }
 
-                if (field.value) {
-                    fieldBlock.classList.remove('form-control_error');
-                    errorFieldBlock.classList.remove('error-message_open');
-                    return true;
-                }
-
-                fieldBlock.classList.add('form-control_error');
-                errorFieldBlock.innerHTML = field.dataset.errorText;
-                errorFieldBlock.classList.add('error-message_open');
-                isValid = false;
-                return false;
-
-            //}
-            //
-            //if (field.type === 'checkbox') {
-            //    let fieldBlock = field.closest('.form-control-checkbox');
-            //    let errorFieldBlock = fieldBlock.querySelector('[data-error-block]');
-            //
-            //    if (field.checked) {
-            //        fieldBlock.classList.remove('form-control_error');
-            //        errorFieldBlock.classList.remove('error-message_open');
-            //        return true;
-            //    }
-            //
-            //    fieldBlock.classList.add('form-control_error');
-            //    errorFieldBlock.innerHTML = field.dataset.errorText;
-            //    errorFieldBlock.classList.add('error-message_open');
-            //    isValid = false;
-            //    return false;
-            //}
-
-
-            //if (field.type === 'radio') {
-            //    let fieldBlock = field.closest('.form-control-group-radio');
-            //    let errorFieldBlock = fieldBlock.querySelector('[data-error-block]');
-            //
-            //    if (field.checked) {
-            //        fieldBlock.classList.remove('form-control_error');
-            //        return true;
-            //    }
-            //
-            //    fieldBlock.classList.add('form-control_error');
-            //    errorFieldBlock.innerHTML = field.dataset.errorText;
-            //    errorFieldBlock.classList.add('error-message_open');
-            //    isValid = false;
-            //    return false;
-            //}
+            fieldBlock.classList.add('form-control_error');
+            errorFieldBlock.innerHTML = field.dataset.errorText;
+            errorFieldBlock.classList.add('error-message_open');
+            isValid = false;
         });
+
+        return isValid;
     }
 }
-
-
-
-
-
-
 
 export {Validate}
--- source/js/pages/works.js
+++ source/js/pages/works.js
@@ -1,3 +1,5 @@
 import {Slider} from '../modules/slider';
+import {Validate} from '../modules/validate';
 
 var slider  = new Slider();
+new Validate();
